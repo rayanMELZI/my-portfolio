2ee684de1807154a5b42e69219e8b6d52aa24cd9
rayanMELZI <rayanmelzi@outlook.com>
2025-04-03 05:42:39 +0100

    Update Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const autoprefixer = require("autoprefixer");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "https://rayanmelzi.github.io/portfolio/",
   // build: {
   //   rollupOptions: {
   //     input: '/public/index.html'
