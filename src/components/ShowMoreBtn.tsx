b75566a0ed41615056a5312e222406e099d307b5
rayanMELZI <rayanmelzi@outlook.com>
2025-04-12 02:08:50 +0100

    Enhance ShowMoreBtn with motion effects for improved interactivity

--- src/components/ShowMoreBtn.tsx
+++ src/components/ShowMoreBtn.tsx
@@ -1,5 +1,6 @@
-import { Plus } from "lucide-react";
 import React from "react";
+import { motion } from "framer-motion";
+import { Plus } from "lucide-react";
 
 type props = {
   topic: "web" | "ux";
@@ -7,7 +8,7 @@ type props = {
 
 function ShowMoreBtn({ topic }: props) {
   return (
-    <a
+    <motion.a
       href={
         topic == "web"
           ? "https://github.com/rayanMELZI"
@@ -15,10 +16,13 @@ function ShowMoreBtn({ topic }: props) {
       }
       target="_blank"
       rel="noopener noreferrer"
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 1 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
       className="block text-4xl text-gray-500 hover:text-indigo-500 bg-gray-200 hover:bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 w-full col-span-2 flex justify-center py-2 rounded-2xl transition-colors duration-300 ease-in-out"
     >
       <Plus size={60} strokeWidth={3} />
-    </a>
+    </motion.a>
   );
 }
 
